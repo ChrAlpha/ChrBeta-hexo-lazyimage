de3fd71febaf605cad7186e0d17f6fe68edd59f1
limhepro <github2@chralpha.com>
2020-03-16 10:56:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ function query(selector) {
 var io = new IntersectionObserver(function (entries) {
     entries.forEach(function (entry) {
         var img = entry.target;
-        if (img.srcset == 'https://cdn.jsdelivr.net/npm/skx@0.0.9/img/lazy.gif') {
-            img.srcset = img.data-srcset;
-        }
+        img.srcset = img.getAttribute('data-srcset');
         io.unobserve(entry);
     });
 });
